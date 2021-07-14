7e9b5fd2c0d2f22aa4133b0a9cad3b9a73ef2dcf
Poommin11 <poommin.ka.61@ubu.ac.th>
2021-07-14 15:54:07 +0700

    Update App.js

--- App.js
+++ App.js
@@ -6,107 +6,55 @@
  * @flow strict-local
  */
  import React, { useState } from "react";
- import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+ import { StyleSheet, Text, TouchableOpacity, View ,TextInput, Button, Picker} from "react-native";
  
  const FlexDirectionBasics = () => {
-   const [flexDirection, setflexDirection] = useState("column");
- 
+   const [a,setA] = useState('');
+   const [b,setB] = useState('');
+   const [selectedValue, setSelectedValue] = useState("plus");
+   const calculate = () => {
+     if(selectedValue=="plus"){
+       var result = parseFloat(a)+parseFloat(b)
+       alert(a+"+"+b+"="+result)
+     } else if(selectedValue=="mins"){
+      var result = parseFloat(a)-parseFloat(b)
+       alert(a+"-"+b+"="+result)
+     } else if(selectedValue=="multiple"){
+      var result = parseFloat(a)*parseFloat(b)
+       alert(a+"x"+b+"="+result)
+     } else if(selectedValue=="divide"){
+      var result = parseFloat(a)/parseFloat(b)
+       alert(a+"/"+b+"="+result)
+     }
+   }
+
    return (
-    <View style={{ flex:1, flexDirection: "column" }}>
-      <View style={{ flex:1,backgroundColor: "pink", flexDirection: "row"}}>
-        <View style={{flex:1,backgroundColor: "blue",justifyContent:"flex-start"}}><Text style={{width: 50, height:50, backgroundColor:"black",margin:10}}>5</Text></View>
-        <View style={{flex:1,flexDirection: "row",backgroundColor: "green",justifyContent:"flex-end"}}><Text style={{width: 50, height:50, backgroundColor:"black",margin:10}}></Text></View>
-        </View>
-      <View style={{ flex:1, flexDirection: "row"}}>
-        <View style={{flex:1,backgroundColor: "yellow",justifyContent:"flex-end"}}><Text style={{width: 50, height:50, backgroundColor:"white",margin:10}}></Text></View>
-        <View style={{flex:1,flexDirection: "column",backgroundColor: "red",justifyContent:"flex-end"}}>
-          <View style={{flexDirection: "row",justifyContent:"flex-end"}}>
-            <Text style={{width: 50, height:50, backgroundColor:"black",margin:10}}></Text>
-          </View>
-          </View>
+     <View>
+          <TextInput 
+          style={{height:50, borderColor:'#000000',borderWidth:1,margin:10}}
+          placeholder="Number"
+          keyboardType="numeric"
+          onChangeText={int=> setA(int)}
+          />
+          <Picker
+          selectedValue={selectedValue}
+          style={{ height: 50, margin:10 }}
+          onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+          >
+          <Picker.Item label="+" value="plus" />
+          <Picker.Item label="-" value="minus" />
+          <Picker.Item label="x" value="multiple" />
+          <Picker.Item label="/" value="divide" />
+          </Picker>
+          <TextInput 
+          style={{height:50, borderColor:'#000000',borderWidth:1,margin:10}}
+          placeholder="Number"
+          keyboardType="numeric"
+          onChangeText={int=> setB(int)}
+          />
+          <Button style={{margin:10}} title="Click me!!!" onPress={calculate}/>
       </View>
-    </View>
+        
    );
  };
- 
- const PreviewLayout = ({
-   label,
-   children,
-   values,
-   selectedValue,
-   setSelectedValue,
- }) => (
-   <View style={{ padding: 10, flex: 1 }}>
-     <Text style={styles.label}>{label}</Text>
-     <View style={styles.row}>
-       {values.map((value) => (
-         <TouchableOpacity
-           key={value}
-           onPress={() => setSelectedValue(value)}
-           style={[
-             styles.button,
-             selectedValue === value && styles.selected,
-           ]}
-         >
-           <Text
-             style={[
-               styles.buttonLabel,
-               selectedValue === value && styles.selectedLabel,
-             ]}
-           >
-             {value}
-           </Text>
-         </TouchableOpacity>
-       ))}
-     </View>
-     <View style={[styles.container, { [label]: selectedValue }]}>
-       {children}
-     </View>
-   </View>
- );
- 
- const styles = StyleSheet.create({
-   container: {
-     flex: 1,
-     marginTop: 8,
-     backgroundColor: "aliceblue",
-   },
-   box: {
-     width: 50,
-     height: 50,
-   },
-   row: {
-     flexDirection: "row",
-     flexWrap: "wrap",
-   },
-   button: {
-     paddingHorizontal: 8,
-     paddingVertical: 6,
-     borderRadius: 4,
-     backgroundColor: "oldlace",
-     alignSelf: "flex-start",
-     marginHorizontal: "1%",
-     marginBottom: 6,
-     minWidth: "48%",
-     textAlign: "center",
-   },
-   selected: {
-     backgroundColor: "coral",
-     borderWidth: 0,
-   },
-   buttonLabel: {
-     fontSize: 12,
-     fontWeight: "500",
-     color: "coral",
-   },
-   selectedLabel: {
-     color: "white",
-   },
-   label: {
-     textAlign: "center",
-     marginBottom: 10,
-     fontSize: 24,
-   },
- });
- 
  export default FlexDirectionBasics;
